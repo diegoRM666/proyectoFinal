4e754ae2afe5412e27f02551e8ef9928bf11f61d
diegoRM666 <al2202000335@azc.uam.mx>
2024-11-14 22:32:50 -0600

    U-14Nov-22:32: Create Obsidian Notes

--- login.py
+++ login.py
@@ -23,7 +23,7 @@ with open('config.yaml', 'r', encoding='utf-8') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
 # Pre-hashing all plain text passwords once
-stauth.Hasher.hash_passwords(config['credentials'])
+# stauth.Hasher.hash_passwords(config['credentials'])
 
 # Creating the authenticator object
 authenticator = stauth.Authenticate(
